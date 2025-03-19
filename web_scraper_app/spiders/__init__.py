404d827376a6a5848bb2b0a9cbd94b78f735c28c
Douglas <douglasbarra11@hotmail.com>
2025-03-18 23:17:51 -0300

    2025-03-18

--- web_scraper_app/spiders/__init__.py
+++ web_scraper_app/spiders/__init__.py
@@ -1,4 +1,14 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
+
+import scrapy
+
+class google(scrapy.Spider):
+    name = "google"
+    allowed_domains = ["example.com"]
+
+    def parse(self, response):
+        for h3 in response.xpath("//h3").getall():
+            yield {"title": h3}
+
+        for href in response.xpath("//a/@href").getall():
+            yield scrapy.Request(response.urljoin(href), self.parse)
+        
